14d783f995885ca2d272aeedb9410c93c42a068c
Kyle Cooper <kyle@webresultsdirect.com>
2023-02-16 14:23:36 +0000

    New block

---
+++ blocks/divider/render.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Renders the divider block.
+ *
+ * @package wrdstudio
+ *
+ * @since 1.0.0
+ */
+
+namespace wrd;
+
+?>
+
+<div <?php block_atts( $block, array( 'class' => 'container py-6' ) ); ?>>
+	<hr class="border-theme-500 border-t-4" />
+</div>
